09d6d3717407dcb8a5acadd11d6f6976a2894eeb
Nicholas Suski <nick@suskitech.org>
2018-11-10 20:45:08 -0700

    add rotation to labels

--- src/text/label.js
+++ src/text/label.js
@@ -7,6 +7,7 @@ export default class {
     font = '16px sans-serif',
     offset = new Vector2(),
     position = new Vector2(),
+    rotation = 0,
     strokeStyle = '#000000',
     text = 'Hello World',
     textAlign = 'center',
@@ -17,36 +18,39 @@ export default class {
     this.font = font;
     this.offset = offset;
     this.position = position;
+    this.rotation = rotation;
     this.shouldFill = shouldFill;
     this.shouldStroke = shouldStroke;
     this.strokeStyle = strokeStyle;
     this.text = text;
     this.textAlign = textAlign;
-    // TODO: Add rotation
   }
 
   draw(canvas, ctx) {
     const screenPos = clipSpaceToPixels(canvas, this.position);
+    const drawPos = new Vector2(
+      screenPos.x + this.offset.x,
+      screenPos.y + this.offset.y,
+    );
 
     ctx.font = this.font;
     ctx.textAlign = this.textAlign;
 
+    // Move canvas to position and rotate
+    ctx.translate(drawPos.x, drawPos.y);
+    ctx.rotate(this.rotation);
+
     if (this.shouldFill) {
       ctx.fillStyle = this.fillStyle;
-      ctx.fillText(
-        this.text,
-        screenPos.x + this.offset.x,
-        screenPos.y + this.offset.y,
-      );
+      ctx.fillText(this.text, 0, 0);
     }
 
     if (this.shouldStroke) {
       ctx.strokeStyle = this.strokeStyle;
-      ctx.strokeText(
-        this.text,
-        screenPos.x + this.offset.x,
-        screenPos.y + this.offset.y,
-      );
+      ctx.strokeText(this.text, 0, 0);
     }
+
+    // Reset transform matrix
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
 }
